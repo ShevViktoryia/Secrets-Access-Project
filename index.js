96c068399f843396ddbf05807e08ff1c08bf2b68
Victoria <shevvictoryia@gmail.com>
2024-09-10 23:15:48 +0300

    feat: add post password

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 //The password is ILoveProgramming
 import express from "express";
+import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
 const app = express();
 const host = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let userIsAuthorised = false;
@@ -24,6 +19,18 @@ function checkPassword(req, res, next) {
 }
 
 app.use(checkPassword);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
 
 app.listen(host, () => {
   console.log("Running on host: " + host);
