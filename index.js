73375730fdf6d77d8cbdd8c54dfe61a56f4a1355
Victoria <shevvictoryia@gmail.com>
2024-09-10 23:01:11 +0300

    feat: add password req

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const app = express();
 const host = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -9,6 +10,20 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let userIsAuthorised = false;
+
+function checkPassword(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+
+app.use(checkPassword);
 
 app.listen(host, () => {
   console.log("Running on host: " + host);
